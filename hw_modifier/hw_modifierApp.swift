f91433d01305ba1ef15190e31fe5ffb3379fcae8
Patty <L224749391@gmail.com>
2020-11-30 12:22:11 +0800

    Initial Commit

---
+++ hw_modifier/hw_modifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  hw_modifierApp.swift
+//  hw_modifier
+//
+//  Created by 林湘羚 on 2020/11/30.
+//
+
+import SwiftUI
+
+@main
+struct hw_modifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
